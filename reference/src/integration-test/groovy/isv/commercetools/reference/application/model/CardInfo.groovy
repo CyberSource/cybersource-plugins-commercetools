ecc33fdd878dad2a578b51c01536800faf7bd801
Carlos <carlosglvn93@gmail.com>
2021-11-17 08:25:52 -0600

    Cleaning master

--- reference/src/integration-test/groovy/isv/commercetools/reference/application/model/CardInfo.groovy
+++
@@ -1,8 +0,0 @@
-package isv.commercetools.reference.application.model
-
-class CardInfo {
-    String cardNumber
-    String cardExpirationMonth
-    String cardExpirationYear
-    String cardType
-}
